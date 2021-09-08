d94e7b7ecd345b3f063e1161049aa09d47ab6346
ricko1993 <rickoyogga@gmail.com>
2021-09-08 21:06:54 +0700

    initial commit

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ semua sintaksis dasar bahasa pemrograman terdiri dari:
 print('Ibu berkata "pergi ketoko"')
 print('Budi menjawab"baik apa yang harus saya lakukan"')
 print('Ibu menjawab "belikan 1 botol susu, dan jika ada telur beli 6"')
-print('maka bui berangkat ketoko')
+print('maka budi berangkat ketoko')
 print('dan mulai belanja')
 
 # Percabangan
-jumlah_botol_susu = 0
+jumlah_botol_susu = 173
+jumlah_telur = 873
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
 
-if jumlah_botol_susu > 0:
+if jumlah_botol_susu > 0 :
     print("budi mengecek harga dan uang cukup")
-    print("budi membeli 1 botol susu")
+    if jumlah_telur == 0 :
+        print("Budi membeli 1 botol susu")
+    else:
+        print("budi membeli 6 botol telur")
+
 else:
     print("budi tidak jadi membeli 1 botol susu")
 
